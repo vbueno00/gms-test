32b0bb1c630fc854b41e4ef110ac69c9c730b9ac
vbueno00 <vic.bueno@outlook.com>
2025-09-27 00:23:41 -0300

    commit

--- cypress/e2e/recomendações.cy.js
+++ cypress/e2e/recomendações.cy.js
@@ -1,16 +1,21 @@
 ///<reference types="cypress"/>
-describe('US-015-Funcionalidade: Lista de recomendações', () => {
-  it('Nome e poster dos filmes aparecendo, 4 a 5 filmes devem aparecer na lista', () => {
+describe('US-015 - Funcionalidade: Lista de recomendações', () => {
+  it('Deve exibir entre 4 e 5 filmes, cada um com nome e poster', () => {
     cy.visit('http://localhost:3000/')
-    cy.get('#recommendations > :nth-child(1) > img').click()
-    cy.get(':nth-child(1) > p').click()
-    cy.get(':nth-child(2) > img').click()
-    cy.get('#recommendations > :nth-child(2) > p').click()
-    cy.get(':nth-child(3) > img').click()
-    cy.get(':nth-child(3) > p').click()
-    cy.get(':nth-child(4) > img').click()
-    cy.get(':nth-child(4) > p').click()
-    cy.get('#recommendations > :nth-child(5) > img').click()
-    cy.get(':nth-child(5) > p').click()
+    cy.get('#recommendations > div')
+    .should('have.length.gte', 4) 
+    .and('have.length.lte', 5)
+    .each(($filme) => {
+      cy.wrap($filme)
+          .find('img')
+          .should('be.visible')
+          .and('have.attr', 'src')
+          .and('match', /\.(jpg|jpeg|png|webp)$/)
+          cy.wrap($filme)
+          .find('p')
+          .should('be.visible')
+          .and('not.be.empty')
+      })
   })
 })
+
